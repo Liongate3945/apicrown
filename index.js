2210f7c335dab088c12cde2d1a536dabfe51582b
Liongate3945 <70343800+Liongate3945@users.noreply.github.com>
2022-07-06 17:52:12 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+var path = require('path');
+var fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    console.log('RE');
+    res.write('lol');
+    res.end();
+  }
+  else if (req.url == '/token') {
+    console.log("EEE");
+    if (req.body && req.body.token != null) {
+      if (req.body.token == process.env['token']) {
+        res.write(JSON.stringify({ validation: "Valid" }));
+        res.end();
+      }
+      else {
+        res.write(JSON.stringify({ validation: "Invalid" }));
+        res.end();
+      }
+    }
+    res.write(JSON.stringify({ validation: "error" }));
+    res.end();
+  }
+});
+
+server.listen(3000)
